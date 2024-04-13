ed11f6137d3067d87da83f52a527f4a19caf11ef
mkaufman385 <mkaufman385@gmail.com>
2024-04-13 15:20:45 -0400

    wip

--- api/jokes.test.js
+++ api/jokes.test.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const db = require("../data/seeds/dbConfig");
 const server = require("../server");
+const Joke = require("./jokesModel");
 
 //DUMBY DATABASE JOKES
 const joke1 = { joke: "Dumby Joke 1", punchline: "Dumby punchline 1" };
@@ -22,4 +23,19 @@ afterAll(async () => {
 
 it("correct env var", () => {
   expect(process.env.DB_ENV).toBe("testing");
+});
+
+describe("jokes model functions", () => {
+  describe("creates joke", () => {
+    it("adds a joke to the db", async () => {
+      let jokes;
+      await Joke.createJoke(joke1);
+      jokes = await db("jokes");
+      expect(jokes).toHaveLength(1);
+
+      await Joke.createJoke(joke2);
+      jokes = await db("jokes");
+      expect(jokes).toHaveLength(2);
+    });
+  });
 });
