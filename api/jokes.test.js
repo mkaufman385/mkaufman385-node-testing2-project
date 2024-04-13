68769bba0c0fed8fc465f2bf5e5c66de7b22522a
mkaufman385 <mkaufman385@gmail.com>
2024-04-13 14:02:32 -0400

    wip

--- api/jokes.test.js
+++ api/jokes.test.js
@@ -2,6 +2,10 @@ const request = require("supertest");
 const db = require("../data/seeds/dbConfig");
 const server = require("../server");
 
+//DUMBY DATABASE JOKES
+const joke1 = { joke: "Dumby Joke 1", punchline: "Dumby punchline 1" };
+const joke2 = { joke: "Dumby Joke 2", punchline: "Dumby punchline 2" };
+
 it("correct env var", () => {
   expect(process.env.DB_ENV).toBe("testing");
 });
